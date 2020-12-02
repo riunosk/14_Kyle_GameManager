08acd31fffc187e0ef9fcb7bf3ff1ebf35c1ace3
riunosk <riunosk@outlook.com>
2020-12-02 16:36:27 +0800

    school

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Character : MonoBehaviour
+{
+    public Animator playerAnim;
+    float moveSpeed = 10;
+    float energyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+		{
+            playerAnim.SetBool("running", true);
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+        if (Input.GetKeyUp(KeyCode.W))
+		{
+            playerAnim.SetBool("running", false);
+		}
+
+        if (Input.GetKey(KeyCode.A))
+		{
+            playerAnim.SetBool("running", true);
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerAnim.SetBool("running", false);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerAnim.SetBool("running", true);
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.SetBool("running", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerAnim.SetBool("running", true);
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerAnim.SetBool("running", false);
+        }
+
+        if (GameManager.instance.levelTime <= 0)
+		{
+            moveSpeed = 0;
+		}
+        GameManager.instance.energyText.text = "Energy : " + energyCount;
+        if (energyCount >= 50)
+		{
+            SceneManager.LoadScene("WinScene");
+		}
+        if (GameManager.instance.levelTime <= 0 || energyCount < 0)
+		{
+            SceneManager.LoadScene("LoseScene");
+		}
+    }
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "AddEnergy")
+		{
+            energyCount += 5;
+            GameManager.instance.levelTime += 10;
+            GameManager.instance.Instantiate4More();
+            Destroy(collision.gameObject);
+		}
+        if (collision.gameObject.tag == "MinusEnergy")
+		{
+            energyCount -= 25;
+            GameManager.instance.levelTime -= 25;
+            Destroy(collision.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+    public GameObject player;
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public int numberOfSpawn;
+    public float levelTime;
+    public Text timerText;
+    public Text energyText;
+    int spawn4 = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+		{
+            instance = this;
+		}
+        for (int i = 0; i < numberOfSpawn; i++)
+        {
+            if (Random.Range(0, 2) < 1)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            else
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelTime > 0)
+		{
+            levelTime -= Time.deltaTime;
+            print("levelTime: " + FormatTime(levelTime));
+		}
+        else
+		{
+            levelTime = 0;
+            print("Time's up!");
+            timerText.text = "Time's up!";
+		}
+        timerText.text = "Timer : " + FormatTime(levelTime);
+    }
+	public string FormatTime(float time)
+	{
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+	}
+    public void Instantiate4More()
+	{
+        for (int i = 0; i < spawn4; i++)
+        {
+            if (Random.Range(0, 2) < 1)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            else
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+    }
+}
